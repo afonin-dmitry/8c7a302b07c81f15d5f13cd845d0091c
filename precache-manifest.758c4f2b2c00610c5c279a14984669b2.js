9dca974bbc1504b6cd6c9ed36e7e6e042a0f2f5f
dmitriy.afonin <dmitriy.afonin@skyeng.ru>
2021-10-24 21:51:56 +0300

    Updates

---
+++ precache-manifest.758c4f2b2c00610c5c279a14984669b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e980c54ed211af438d976dc8bbeccee7",
+    "url": "/8c7a302b07c81f15d5f13cd845d0091c/index.html"
+  },
+  {
+    "revision": "21cae879963261394afc",
+    "url": "/8c7a302b07c81f15d5f13cd845d0091c/static/css/2.e3cf4cf6.chunk.css"
+  },
+  {
+    "revision": "de4ff4e446fc57e48f38",
+    "url": "/8c7a302b07c81f15d5f13cd845d0091c/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21cae879963261394afc",
+    "url": "/8c7a302b07c81f15d5f13cd845d0091c/static/js/2.d8ee2fb7.chunk.js"
+  },
+  {
+    "revision": "82bff80a650a893b1d4d0751dc591bf2",
+    "url": "/8c7a302b07c81f15d5f13cd845d0091c/static/js/2.d8ee2fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4ff4e446fc57e48f38",
+    "url": "/8c7a302b07c81f15d5f13cd845d0091c/static/js/main.c24f8cca.chunk.js"
+  },
+  {
+    "revision": "cc097bb95bcb5a7ee5db",
+    "url": "/8c7a302b07c81f15d5f13cd845d0091c/static/js/runtime-main.aab85845.js"
+  }
+]);
